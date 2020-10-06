fe8dad1fc8b845059fdb900a9cfa1954b67216df
Ceren <cerenbulbul27@gmail.com>
2020-10-06 09:21:56 -0700

    bora part

--- src/components/HomePage/JustDefault.js
+++ src/components/HomePage/JustDefault.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Title from '../Title';
 import Image from "../../images/bora.jpeg";
+import Image2 from "../../images/bora2.png";
+import Image3 from "../../images/bora3.png";
+import Carousel from "react-bootstrap/Carousel";
 
 export default function JustDefault() {
     return (
@@ -17,7 +20,29 @@ export default function JustDefault() {
                        
                    </div>
                    <div className="just-video">
-                    <img src={Image} width="500" height="300" />
+                   <div className="inside-container">
+                        <div>
+                            <Carousel>
+                            <Carousel.Item>
+                                <img className="d-block w-100" src={Image} alt="First slide" />
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                className="d-block w-100"
+                                src={Image2}
+                                alt="Third slide"
+                                />
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                className="d-block w-100"
+                                src= {Image3}
+                                alt="Third slide"
+                                />
+                            </Carousel.Item>
+                            </Carousel>
+                        </div>
+                        </div>
                    </div>
                </div>
            </div>
